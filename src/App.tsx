9cd9e7c3f6322d53c583ac7d02f7a5be6a01bac4
OshadhaShiro <shirokumara49@gmail.com>
2021-11-14 10:56:23 +0530

    setup pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from "react";
-import { Routes as Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Navbar from "./Components/Navbar";
 
 const App = () => {
   return (
     <>
-      <Switch>
+      <Navbar />
+      <Routes>
         <Route path="/" element={<Home />} />
-      </Switch>
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
     </>
   );
 };
